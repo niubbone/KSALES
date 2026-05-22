7593d9215cace9f0e15e89630f22c6c736d22cab
niubbone <guren@tiscali.it>
2026-05-22 17:03:58 +0200

    config: aggiorna URL Web App v3

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   // URL del GAS Web App — sostituire dopo il primo deploy
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbwrHmnOuU9pUkW9ifeoMkBlggT1EQku6LATp0a55rMdmUwuNj7r9h4jj2Qv-qUkWx7p/exec',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyX1LAp2dGvRnmkH24NiEO8X6CX5chZqok0bcHn30XRPf3y8f3qZFmEjtabXD5fBmzm/exec',
   APP_NAME: 'KSales',
   VERSION: '0.1.0'
 };
