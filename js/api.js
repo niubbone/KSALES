bc23b9a2c2ec0b432ad0ccb9a187fcfd4e34319a
niubbone <guren@tiscali.it>
2026-05-22 17:03:42 +0200

    feat: navigazione cliente/contratto + dettaglio economico con chart

--- js/api.js
+++ js/api.js
@@ -2,16 +2,16 @@ async function apiCall(action, params) {
   var url = new URL(CONFIG.APPS_SCRIPT_URL);
   url.searchParams.set('action', action);
   if (params) Object.keys(params).forEach(function(k) { url.searchParams.set(k, params[k]); });
-
-  var res = await fetch(url.toString(), { redirect: 'follow' });
+  var res  = await fetch(url.toString(), { redirect: 'follow' });
   var data = await res.json();
-  if (data.error) throw new Error(data.error);
+  if (data && data.error) throw new Error(data.error);
   return data;
 }
 
 var API = {
-  getContratti: function() { return apiCall('getContratti'); },
-  getClienti:   function() { return apiCall('getClienti');   },
-  getProdotti:  function() { return apiCall('getProdotti');  },
-  getAgenzie:   function() { return apiCall('getAgenzie');   }
+  getContratti:       function()         { return apiCall('getContratti'); },
+  getClienti:         function()         { return apiCall('getClienti'); },
+  getProdotti:        function()         { return apiCall('getProdotti'); },
+  getAgenzie:         function()         { return apiCall('getAgenzie'); },
+  getContrattoDetail: function(numOrdine){ return apiCall('getContrattoDetail', { num_ordine: numOrdine }); }
 };
